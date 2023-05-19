2e5bb5354e1a4913e1b20d236dbd458e655006fa
Pranesh Deshmukh <praneshd2003@gmail.com>
2023-05-20 00:58:59 +0530

    four.sql

--- Questions  /four.sql
+++ Questions  /four.sql
@@ -1,3 +1,5 @@
+https://www.hackerrank.com/challenges/weather-observation-station-3/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
 Query a list of CITY names from STATION for cities that have an even ID number. 
 Print the results in any order, but exclude duplicates from the answer.
 
